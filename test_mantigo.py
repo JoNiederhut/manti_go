84cc402e387d77b1871b6a0a6fb2ad1c384fc7b5
Kristian Rother <krother@academis.eu>
2022-09-28 23:14:45 +0200

    improved tests for MantiGo class

--- test_mantigo.py
+++ test_mantigo.py
@@ -10,54 +10,81 @@ TDD Cycle (Test Driven Development)
 3. repeat
 """
 
-from logging import _Level
 import time
-from webbrowser import get
 import mantigo as mtg
+import pytest
 
-def test_mantigo_class():
-    game= mtg.MantiGo()
 
-def test_mantigo_timer():
-    game = mtg.MantiGo(time_limit=(0,2)) # 0 min 2 sec
+TEST_LEVEL = """
+WWWWWWWWWW
+W       EW
+W        W
+W        W
+W     m  W
+W m      W
+WWWWWWWWWW
+"""
+
+
+def count_symbols(game, symbol):
+    """helper function for counting symbols"""
+    return len([s for y, x, s in game.get_symbols() if s == symbol])
+
+
+def get_player_position(game):
+    """helper function finding the player symbol"""
+    for y, x, symbol in game.get_symbols():
+        if symbol == 'player':
+            return y, x
+
+
+@pytest.fixture
+def game():
+    """helper function preparing a MantiGo object for testing"""
+    return mtg.MantiGo(
+        time_limit=(0, 2),  # 0 min 2 sec
+        level=TEST_LEVEL,
+        player_start=(2, 2)
+    )
+
+
+def test_mantigo_timer(game):
+    """Waiting for too long loses the game"""
     assert game.status == "running"
     time.sleep(2.1)
     assert game.status == "Game Over"
 
 
-TEST_LEVEL = """
-WWWWWWWWWW
-W        W
-W        W
-W        W
-W        W
-W        W
-WWWWWWWWWW
-""".strip().split("\n")
+def test_get_symbols(game):
+    """The get_symbols method returns a long list of symbols"""
+    # count symbols on the test map
+    assert count_symbols(game, 'W') == 34
+    assert count_symbols(game, 'E') == 1
+    assert count_symbols(game, 'm') == 2
+    assert count_symbols(game, 'player') == 1
+    # do not count enemies, because they are placed randomly
 
 
-def test_player_move():
-    game= mtg.MantiGo(
-        time_limit=(0,1),
-        level=TEST_LEVEL,
-        player_start=(3,3)
-    )
-    assert game.get_map() == """
-WWWWWWWWWW
-W        W
-W        W
-W  P     W
-W        W
-W        W
-WWWWWWWWWW
-"""
-    game.player_command("right")
-    assert game.get_map() == """
-WWWWWWWWWW
-W        W
-W        W
-W   P    W
-W        W
-W        W
-WWWWWWWWWW
-"""
+def test_player_move(game):
+    """Moving changes the player position"""
+    assert get_player_position(game) == (2, 2)
+    game.move("right")
+    assert get_player_position(game) == (2, 3)
+    game.move("down")
+    assert get_player_position(game) == (3, 3)
+    game.move("left")
+    assert get_player_position(game) == (3, 2)
+    game.move("up")
+    assert get_player_position(game) == (2, 2)
+
+
+def test_player_move_into_wall(game):
+    """When there is a wall, no movement is possible"""
+    assert get_player_position(game) == (2, 2)
+    game.move("left")
+    assert get_player_position(game) == (2, 2)
+
+
+def test_update(game):
+    """update() can be called. Not testing details here"""
+    game.update()
